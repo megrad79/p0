b27e035f6912427a4ea0bdc1fa0ff2b356266088
Taryn Nicole <megrad.79@gmail.com>
2021-07-27 09:48:58 -0400

    Pseudo code of Lightning Tracker
    - modeling of the project 0
    - need to work on this more instead of debugging other issues
    
    To Do - Tues., July 27th:
    - add main class
    - find appropriate data to insert
    - import to function

---
+++ psuedo_LT.java
@@ -0,0 +1,54 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// NAME: Taryn Nicole Jones ----------------------------------------------------------------------------------------
+// COURSE: Revature - Java Reactive --------------------------------------------------------------------------------
+// DATE: 7/23/2021 ---------------------------------------------------------------------------------------------------
+// TITLE: Project 0 -----------------------------------------------------------------------------------------------------
+// DESCRIPTION: Lightning Predictor App ---------------------------------------------------------------------------
+// Goals: ---------------------------------------------------------------------------------------------------------------
+// ----- 1. predict next lightning stirke given data -------------------------------------------------------------------
+// ----- 2. predict lightning in given area, given history trends (possibly try to get all in US based on zipcode) --
+// ----- 3. output lightning map through time -----------------------------------------------------------------------
+// ----- 4. output lightning heat map ---------------------------------------------------------------------------------
+// ----- >>> BONUS: Can also try predicting the magnitude + landfall + potential warnings? --------------------
+
+Lightning Predictor App:
+1. predict next lightning stirke given data
+2. predict lightning in given area, given history trends (possibly try to get all in US based on zipcode)
+3. output lightning map through time
+4. output lightning heat map
+>>> Can also try predicting the magnitude + landfall + potential warnings?
+
+// headers
+
+//class???
+public static void main(Strings[] args){
+	// Make a program that can output data to make sure loaded well from source
+	// Follow the logic from Rstudio data population - QMB 5565
+	// Can even run there to ensure running as expected
+
+	System.out.println();
+}
+
+
+// Function that creates multi-lienear regression to predict next lightning strike
+// Explain why important and useful
+// ****** BONUS: see if can compare throughout the day ****
+// Z-scores, p value etc.
+
+
+// Function that creates an animated map/ gif of lightning outputs
+// Explain why important and useful
+// ****** BONUS: see if can compare times throughout the day (decide upon frequency) ****
+// ****** BONUS: future map - use averages of last 3 years in a month/day/time ****
+
+
+// Function that creates heat map of lightning outputs
+// Explain why important and useful
+// ****** BONUS: see if can compare on 3 year time frame photos ****
+// ****** BONUS: future map - use averages of last 3 years in a month/day/time ****
+
+
+// Impreovemnts:
+// Make fast to load data
+// Make fast to quickly find data to predict
+// Make look nice, fancy for user interface (still on command line)
