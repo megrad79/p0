c3174dbc062439e041d50b646a7732a0403d3536
Taryn Nicole <megrad.79@gmail.com>
2021-08-01 10:42:01 -0400

    Create scoring.java (increment/ decrement example)

---
+++ javaPractice/scoring.java
@@ -0,0 +1,20 @@
+// SoloLearn Who Scored More? Program
+
+import java.util.Scanner;
+
+public class Main {
+
+   public static void main(String[] args) {
+       
+       Scanner scanner = new Scanner(System.in);
+       //taking initial score
+       int initScore = scanner.nextInt();
+       int scoreTom = initScore;
+       int scoreBob = initScore;
+       
+       System.out.println("Round 1 results:");
+       //fix
+       System.out.println(++scoreTom);
+       System.out.println(--scoreBob);
+   }
+}
