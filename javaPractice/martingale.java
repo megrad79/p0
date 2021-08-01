300bde2f94e950c4ce9b1126da3741c72f250538
Taryn Nicole <megrad.79@gmail.com>
2021-08-01 10:22:41 -0400

    Create martingale.java

---
+++ javaPractice/martingale.java
@@ -0,0 +1,22 @@
+// Udacity martingale betting technique
+public int martingale() {
+ int money = 1000;
+ int target = 1200;
+ int bet = 10;
+ while (money > bet) {
+  boolean win = play();
+  if (win) {
+   money += bet;
+   bet = 10;
+  } 
+  else {
+   money -= bet;
+   bet *= 2;
+  }
+  
+ if(money >= target){
+     break;
+ }
+ }
+ return money;
+}
